44a2d8aa0be37e5f64e7aa96555af052a725cf6e
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-28 10:58:19 -0300

    feat: carousel

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,64 @@
-// const names: Array<string> = []; // string[]
-// // names[0].split(' ');
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+document.addEventListener('DOMContentLoaded', () => {
+  let currentSlide: number = 0;
 
-// const promise: Promise<number> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10);
-//   }, 2000);
-// });
+  const pagination: HTMLElement = document.querySelector('.pagination')!;
+  const slides: HTMLElement = document.querySelector('.slides')!;
+  const totalSlides: number = document.querySelectorAll('.slideContainer').length;
 
-// promise.then(data => {
-//   // data.split(' ');
-// })
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
 
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
-
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj);
-
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = 'Got no value.';
-  if (element.length === 1) {
-    descriptionText = 'Got 1 element.';
-  } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.';
-  }
-  return [element, descriptionText];
-}
-
-console.log(countAndDescribe(['Sports', 'Cooking']));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
-
-extractAndConvert({ name: 'Max' }, 'name');
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
+  for (let i = 0; i < totalSlides; i++) {
+    const bar = document.createElement('div');
+    bar.classList.add('bar');
+    bar.setAttribute('data-slide', `${i}`);
+    pagination.appendChild(bar);
   }
 
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) {
-      return;
+  const selectBar = (selectedBar: Element) => {
+    bars.forEach(bar => {
+      bar.classList.remove('selectedBar');
+    })
+    selectedBar.classList.add('selectedBar');
+  }
+
+  const bars = document.querySelectorAll('.bar');
+  bars[0].classList.add('selectedBar');
+  bars.forEach(bar => {
+    bar.addEventListener('click', () => {
+      const slideIndex = bar.getAttribute('data-slide')!;
+      selectBar(bar)
+      showSlide(parseInt(slideIndex));
+    });
+  });
+
+  if (prevButton && nextButton) {
+    prevButton.addEventListener('click', () => changeSlide(-1));
+    nextButton.addEventListener('click', () => changeSlide(1));
+  }
+
+
+  function changeSlide(n: number): void {
+    showSlide(currentSlide += n);
+  }
+
+  function showSlide(n: number): void {
+
+    if (n >= totalSlides) {
+      currentSlide = 0;
+    } else if (n < 0) {
+      currentSlide = totalSlides - 1;
+    } else {
+      currentSlide = n;
     }
-    this.data.splice(this.data.indexOf(item), 1); // -1
+    selectBar(bars[currentSlide])
+    const slideElement: HTMLElement = document.querySelector('.slideContainer')!
+    const slideWidth: number = slideElement.offsetWidth;
+    slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
   }
 
-  getItems() {
-    return [...this.data];
-  }
-}
-
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
-
-const numberStorage = new DataStorage<number>();
-
-// const objStorage = new DataStorage<object>();
-// const maxObj = {name: 'Max'};
-// objStorage.addItem(maxObj);
-// objStorage.addItem({name: 'Manu'});
-// // ...
-// objStorage.removeItem(maxObj);
-// console.log(objStorage.getItems());
-
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
-}
-
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['Max', 'Anna'];
-// names.push('Manu');
-// names.pop();
+});
